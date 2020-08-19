f4d4cb50a5b4f94073bb04bb0f9ebfa1b5f0de80
Ru <rudikraeher@gmail.com>
2020-08-18 17:43:42 -0700

    created working randomizer functions for the arrays that combines all character criteria and a shuffler for the resultant combined array

--- js/script.js
+++ js/script.js
@@ -1,22 +1,98 @@
-//Password criteria
+
+
+//Variables
 
 var ranUpper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]; 
 var ranLower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var ranNumber = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 var ranSpecial = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "-", "=", "+", "[", "]", "{", "}", "|", "?", "'", ",", "<", ">", ".", "/", "`", "~", ";", ":",];
+var password = [];
+var charInput = ["u", "l", "n", "s", "all"];
 
-//Random functions
+// if (charInput.indexOf(types) !== -1) {
+//     console.log(types);
+// }
 
-var randomizeU = ranUpper[Math.floor(Math.random()* ranUpper.length)];
-var randomizeL = ranLower[Math.floor(Math.random()* ranLower.length)];
-var randomizeN = ranNumber[Math.floor(Math.random()* ranNumber.length)];
-var randomizeS = ranSpecial[Math.floor(Math.random()* ranSpecial.length)];
- 
- //String concatenation // var namesTogether = "My pet name is " + random + " " + random2;
 
-//How to combine but in a random order; I need to randomize the string concatenation. One possible way to do this is to store the randoms in an array and then randomly
-//pull from that array to generate the password characters. 
 
+
+
+
+//Functions
+
+function randomizeU() {
+    return ranUpper[Math.floor(Math.random()* ranUpper.length)];
+  }
+
+function randomizeL() {
+    return ranLower[Math.floor(Math.random()* ranLower.length)];
+}
+
+function randomizeN() {
+    return ranNumber[Math.floor(Math.random()* ranNumber.length)];
+}
+
+function randomizeS() {
+    return ranSpecial[Math.floor(Math.random()* ranSpecial.length)];
+}
+
+function securePassword(array) {        //This is my array shuffler to rearrange the output of the criteria function. 
+    var currentIndex = array.length, temporaryValue, randomIndex;
+    while (0 !== currentIndex) {
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1;
+      temporaryValue = array[currentIndex];
+      array[currentIndex] = array[randomIndex];
+      array[randomIndex] = temporaryValue;
+    }
+    return array;
+  }
+
+function criteria() {
+//   var types = prompt("Select the character types that you want in your password. Type 'u' for uppercase, 'l' for lowercase, and 's' for special characters. Type 'all' to include all character types."); 
+//   types.toLowerCase(); //This used to be inside function. Now I am moving it outside. 
+
+  if (types.includes("u")) {
+      randomizeU;
+      password.push(randomizeU());
+  } 
+  if (types.includes("l")) {
+      randomizeL;
+      password.push(randomizeL());
+  }
+  if (types.includes("s")) {
+      randomizeS;
+      password.push(randomizeS());
+  }
+  if (types.includes("n")) {
+      randomizeN;
+      password.push(randomizeN());
+  }
+  if (types.includes("all")) { //!!Bugged. At the moment, randomize all returns password.length+2. One way to deal with this would be to create another array that has ALL the characters in it and just do the same randomize function as above. 
+      randomizeU;
+
+      randomizeL;
+    //   password.push(randomizeL());
+      randomizeS;
+    //   password.push(randomizeS());
+      randomizeN;
+    //   password.push(randomizeN());
+    password.push(randomizeU(), randomizeL(), randomizeS(), randomizeN());
+    password.pop();
+  }
+//   if (password.length === pwLength) {
+//       return console.log(password);
+//   }
+
+
+
+//   } else {                  //!!This is bugged. Won't allow user to try again data.
+//            alert("Please select valid critera."); 
+//       }
+
+console.log(password);
+    
+  } 
 
 
 
@@ -25,28 +101,55 @@ alert("Welcome to Ru's password generator site. Press the 'Generate Password' bu
 
 // Add event listener to generate button
 var generateBtn = document.querySelector("#generate");
-generateBtn.addEventListener("click", criteria); 
+generateBtn.addEventListener("click", criteria); //I think I need to replace criteria function make it so that clicking the button, instead of starting the program like it did before
+//now it works to display the password that I have randomly generated. It needs to be a function, not criteria - the function that will display the pw. This isn't quite how I wanted it
+//to work but for now I can have it work this way. Also revise my welcome alert text. 
 
-function criteria() {
 
-    var length = prompt("Select a length for your password. Password must be at least 8 characters and no more than 128 characters.");
+var pwLength = prompt("Select a length for your password. Password must be at least 8 characters and no more than 128 characters.");
      //Only proceed if user selects a valid length
-    if (length >= 8 && length <= 128) {
+
+    if (pwLength < 8 || pwLength > 128) {
+        alert("Please select a valid length");
+        pwLength;
+
+    } else {    
         var types = prompt("Select the character types that you want in your password. Type 'u' for uppercase, 'l' for lowercase, and 's' for special characters. Type 'all' to include all character types."); 
-    
-        //TODO: Eventually I want any combination to be possible. For example only lowercase and special. Also, user must select at least one type to proceed.
-        if (types === "u" || types === "l" || types === "s" || types === "all") {
-            // while (password.length <= length) { //TODO: Still working on the randomizer. 
-            //     function generator() {
-            //         var password = randomizeU + randomizeL + randomizeN + randomizeS;
-            //         password.push();
-            //         console.log(password);
-            //     } 
-            // }
+        types.toLowerCase();
+        while (password.length < pwLength) { 
+            criteria();
         }
+          
+        
+        //Run the criteria function.
+        // var types = prompt("Select the character types that you want in your password. Type 'u' for uppercase, 'l' for lowercase, and 's' for special characters. Type 'all' to include all character types."); 
+        // types.toLowerCase();
+
+        // for (let i = 0; i < pwLength; i++) { //Try putting the loop here
+        //     password.push //I think the ENTIRE combined output of the various randomizers needs to go here. Perhaps I make that the criteria function so I push the criteria function to the password var. 
+
+        // if (types.includes("u")) {
+        //     randomizeU;
+        //       for (let i = 0; i < pwLength; i++) { //*This loop may have to go on the entire function. I need it to populate the array with all the selected elements and for the whole thing to have a length that corresponds to the user's input
+        //         password.push(randomizeU());
+        //       }
+        //       console.log(password);
+        //     //   shuffle(password);
+        // } 
+        // if (types.includes("l")) {
+        //     randomizeL;
+        //     for (let i = 0; i < pwLength; i++) { 
+        //         password.push(randomizeL());
+        //       }
+        //       console.log(password);
+        //     //   shuffle(password);
+        
+        //     } else {
+        //         alert("Please select valid critera.");
+        //     }
+        // } 
     }
-  
-}
+// } original ending bracket for criteria function
  
 
 // Write password to the #password input
@@ -65,14 +168,14 @@ function writePassword() {
 //click a button to generate a password 
 //After clicking button, user is presented with a series of prompts that enables the user to select which criteria should be in the password.
 //Prompt for length that is at least 8 characters and no more than 128
-//A prompt for character types -- should these be separate prompts for upper, lower, and special characters? Or one that is just like type: u, l, and s (or any combo thereof) to 
-    //include each of those types
+//A prompt for character types and allow any combination 
 //After all prompts a pw is generated that matches the criteria
 //The pw is displayed in an alert or written to the page. 
 
 
 
 //Notes
-//Unsure about the meaning of this in the acceptance criteria: 
-//WHEN I answer each prompt
-//THEN my input should be validated and at least one character type should be selected
+//Ok so now I have to have a function for each of the types, that gets added into the randomizer.
+//I will also need an array shuffle for the final password. 
+//I need to allow any combination of criteria
+ 
